b0d199d5f2d9e9dabd6882ed69965f12a6f5b352
Mojtaba Mosavi <mosevi.m@yahoo.com>
2021-06-08 23:04:55 +0200

    compeleted the info section

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,12 @@
 const nav = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".header__nav");
+const body = document.getElementsByTagName("body")[0];
 
 function toggleMenu(){
     nav.classList.toggle("hamburger-menu--active");
     menu.classList.toggle('header__nav--active');
+    body.classList.toggle("no-scroll");   
+
 }
 
 
